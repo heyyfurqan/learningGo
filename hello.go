ec0bb000756a6dd70bfb0a670062b6bf3307f6ff
Furqan Abid <thisismefurqan@gmail.com>
2023-09-09 18:55:50 +0500

    functions

--- hello.go
+++ hello.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+func add(a int, b int) int {
+	return a + b
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	//fmt.Printf("Hello, World!\n")
+	fmt.Printf("Hello, You have %g problems.\n", math.Sqrt(7))
+	//fmt.Println("Pie's value is ", math.Pi)
+	fmt.Println(add(13, 12))
 }
