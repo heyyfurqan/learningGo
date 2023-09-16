8f44325605aeeb130c63f1e8d4a4bc8880bec8a9
Furqan Abid <thisismefurqan@gmail.com>
2023-09-16 16:22:06 +0500

    variables

--- hello.go
+++ hello.go
@@ -5,6 +5,14 @@ import (
 	"math"
 )
 
+var j int = 10
+
+func split(sum int) (a, b int) {
+	a = sum * 5 / 8
+	b = sum - a
+	return
+}
+
 func add(a, b int) int {
 	return a + b
 }
@@ -15,7 +23,16 @@ func swap(a, b string) (string, string) {
 
 func main() {
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+
 	fmt.Println(math.Pi)
+
 	fmt.Println(add(44, 55))
-	fmt.Println(swap("hello", "nigga"))
+
+	c, d := swap("hello", "nigga")
+	fmt.Println(c, d)
+
+	fmt.Println(split(25))
+
+	var cpp, js, react = true, false, "slow AF!"
+	fmt.Println(j, cpp, js, react)
 }
